444dd82e2a8c5f433c01ccde2a8d1833782d09cc
Akhil Pragallapati <pragallapatiakhil@gmail.com>
2023-06-18 23:28:03 -0400

    Removing the TDS extra folder

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-This directory contains the entire code for the Resaerch Article search 
+This directory contains the entire code for the Research Article Search Engine 
 
--- TDSProject/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from TDSProject.models import DB1
-from TDSProject.models import Q
-from TDSProject.models import operators
-
-admin.site.register(DB1)
-admin.site.register(Q)
-admin.site.register(operators)
